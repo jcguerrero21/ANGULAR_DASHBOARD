662f73062383e2be34231a38d93200ff2375a7e8
juan carlos <jotaceguerrerom21@gmail.com>
2019-10-26 19:16:01 +0200

    mejoras

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loadAPI: Promise<any>;
+    
+
   constructor() { }
 
   ngOnInit() {
+    this.loadAPI = new Promise((resolve) => {
+      this.loadScript();
+      resolve(true);
+    });
+  }
+
+  async loadScript() {
+    var dynamicScripts = ["../../../../assets/js/login.js"];
+
+    for (var i = 0; i < dynamicScripts.length; i++) {
+      let node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+
   }
 
 }
